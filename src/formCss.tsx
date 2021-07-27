713bbfadea68012d7cc2e45175b8f98c648ceb52
ShiunMurata <purpleclouds0916@gmail.com>
2021-07-27 18:22:58 +0900

    スタイルとDOM操作のためにクラスの適用

---
+++ src/formCss.tsx
@@ -0,0 +1,134 @@
+export const formCss = {
+  classNames: 'form-items',
+  YieldModelType: {
+    classNames: 'YieldModelType',
+  },
+  SAType: {
+    classNames: 'SAType test',
+  },
+  SDMD: {
+    classNames: 'SDMD',
+    H: {
+      classNames: 'H-items',
+      items: {
+        classNames: 'H-input',
+      },
+    },
+    V: {
+      classNames: 'V-items',
+      items: {
+        classNames: 'V-input',
+      },
+    },
+    NRF: {
+      classNames: 'NRF-input',
+    },
+    DBH: {
+      classNames: 'DBH-items',
+      items: {
+        classNames: 'DBH-input',
+      },
+    },
+    HF: {
+      classNames: 'HF-items',
+      items: {
+        classNames: 'HF-input',
+      },
+    },
+  },
+  Density: {
+    classNames: 'Density Management',
+    Plant: {
+      classNames: 'Plant-items',
+      items: {
+        classNames: 'Plant-input',
+      },
+    },
+    Minimum: {
+      classNames: 'Minimum-input',
+    },
+  },
+  RegenerationCost: {
+    classNames: 'RegenerationCost Management pan',
+    items: {
+      classNames: 'RegenerationCost-items pan',
+    },
+  },
+  ThinningPercent: {
+    classNames: 'ThinningPercent Management',
+  },
+  AnnualInterestPercent: {
+    classNames: 'AnnualInterestPercent Management',
+  },
+  HarvestingAges: {
+    classNames: 'HarvestingAges Management',
+  },
+  MaxNumOfHarvest: {
+    classNames: 'MaxNumOfHarvest Management',
+  },
+  Thinning: {
+    classNames: 'Thinning form-card-item',
+    YieldRate: {
+      classNames: 'YieldRate Thinning-item',
+    },
+    Cost: {
+      classNames: 'Cost Thinning-item Thinning-flex-item',
+    },
+    StumpHeight: {
+      classNames: 'StumpHeight Thinning-item Thinning-flex-item',
+    },
+    LogLength: {
+      classNames: 'LogLength Thinning-item Thinning-flex-item',
+    },
+    LoggingPitch: {
+      classNames: 'LoggingPitch Thinning-item Thinning-flex-item',
+    },
+    Diameter: {
+      classNames: 'Diameter Thinning-item',
+      items: {
+        classNames: 'Diameter-items',
+      },
+    },
+    Price: {
+        classNames: 'Price Thinning-item',
+        items: {
+          classNames: 'Price-items',
+        },
+      },
+  },
+  Clearcut: {
+    classNames: 'Clearcut form-card-item',
+    YieldRate: {
+      classNames: 'YieldRate Clearcut-item',
+    },
+    Cost: {
+      classNames: 'Cost Clearcut-item Clearcut-flex-item',
+    },
+    StumpHeight: {
+      classNames: 'StumpHeight Clearcut-item Clearcut-flex-item',
+    },
+    LogLength: {
+      classNames: 'LogLength Clearcut-item Clearcut-flex-item',
+    },
+    LoggingPitch: {
+      classNames: 'LoggingPitch Clearcut-item Clearcut-flex-item',
+    },
+    Diameter: {
+      classNames: 'Diameter Clearcut-item',
+      items: {
+        classNames: 'Diameter-items',
+      },
+    },
+    Price: {
+        classNames: 'Price Clearcut-item',
+        items: {
+          classNames: 'Price-items',
+        },
+      },
+  },
+  SA: {
+    classNames: 'Simulated_Annealing',
+  },
+};
+
+export default formCss;
