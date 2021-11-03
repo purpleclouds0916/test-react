4e1c5141af2bdc3a6fecdbff73fa1fa5035c4670
Shiun Murata <purpleclouds0916@gmail.com>
2021-11-03 12:11:57 +0900

    windowsでJavaをインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { VFC } from 'react';
 
 // import Test from './components/pages/Test';
-// import Test1 from './components/pages/Test1';
+import Test1 from './components/pages/Java';
 import Form from './components/pages/Form';
 
 
 const App: VFC = () => (
   <div className="App">
-    {/* <Test1 /> */}
+    <Test1 />
    <Form/>
   </div>
 );
