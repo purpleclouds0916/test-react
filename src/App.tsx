5b50637756ff72df34209a247e6629e0c4c8da54
Shiun Murata <purpleclouds0916@gmail.com>
2021-11-24 10:19:54 +0900

    eslintとprettierの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { VFC } from 'react';
 
 // import Test from './components/pages/Test';
-import Test1 from './components/pages/Java';
 import Form from './components/pages/Form';
 
 
 const App: VFC = () => (
   <div className="App">
-    <Test1 />
    <Form/>
   </div>
 );
